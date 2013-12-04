0e0b559c7e2f293439f609db81ced3957a3382a8
Joah Gerstenberg <joahd@gbergfamily.com>
2013-12-04 14:55:41 -0600

    Fixes turbolinks-related bug, closes #4

--- dist/obfuscate.js
+++ dist/obfuscate.js
@@ -1,11 +1,17 @@
 (function() {
-    $(document).ready(function(){
-    	$.each($('[data-obfuscated]'), function(i,v) {
-	        $(v).text(decodeURIComponent($(v).attr('data-obfuscated')))
+	unObfuscate = function() {
+		$.each($('[data-obfuscated]'), function(i,v) {
+	        $(v).text(decodeURIComponent($(v).attr('data-obfuscated')));
 		});
 		$.each($('[data-obfuscated-email]'), function(i,v) {
-			ema = decodeURIComponent($(v).attr('data-obfuscated-email'))
-			$(v).text($(v).attr('data-obfuscated-message').length > 0 ? decodeURIComponent($(v).attr('data-obfuscated-message')) : ema).attr('href', 'mailto:'+ema+($(v).attr('data-obfuscated-subject').length > 0 ? '?subject='+decodeURIComponent($(v).attr('data-obfuscated-subject')).replace(' ','%20') : '') + ($(v).attr('data-obfuscated-subject').length > 0 && $(v).attr('data-obfuscated-body').length > 0 ? '&body='+decodeURIComponent($(v).attr('data-obfuscated-body')).replace(' ','%20') : $(v).attr('data-obfuscated-body').length > 0 ? '?body='+decodeURIComponent($(v).attr('data-obfuscated-body')).replace(' ','%20') : ''))
-		})
+			ema = decodeURIComponent($(v).attr('data-obfuscated-email'));
+			$(v).text($(v).attr('data-obfuscated-message').length > 0 ? decodeURIComponent($(v).attr('data-obfuscated-message')) : ema).attr('href', 'mailto:'+ema+($(v).attr('data-obfuscated-subject').length > 0 ? '?subject='+decodeURIComponent($(v).attr('data-obfuscated-subject')).replace(' ','%20') : '') + ($(v).attr('data-obfuscated-subject').length > 0 && $(v).attr('data-obfuscated-body').length > 0 ? '&body='+decodeURIComponent($(v).attr('data-obfuscated-body')).replace(' ','%20') : $(v).attr('data-obfuscated-body').length > 0 ? '?body='+decodeURIComponent($(v).attr('data-obfuscated-body')).replace(' ','%20') : ''));
+		});
+	};
+    $(document).ready(function(){
+		unObfuscate();
+    });
+    $(document).on('page:change', function() {
+    	unObfuscate();
     });
 })();
